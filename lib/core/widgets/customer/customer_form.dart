5933a637c32a9d647f9199e292334f6c489af1d8
Fatih Kurt <kurf061@gmail.com>
2024-11-10 23:20:01 +0300

    add new customer form edited

--- lib/core/widgets/customer/customer_form.dart
+++ lib/core/widgets/customer/customer_form.dart
@@ -24,100 +24,103 @@ class _CustomerFormState extends State<CustomerForm> {
   Widget build(BuildContext context) {
     return Form(
       key: widget.formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // TextFormField(
-          //   controller: widget.nameController,
-          //   decoration: const InputDecoration(
-          //     labelText: 'İsim',
-          //     prefixIcon: Icon(Icons.person),
-          //   ),
-          //   validator: (value) {
-          //     if (value == null || value.isEmpty) {
-          //       return 'Lütfen bir isim girin';
-          //     }
-          //     return null;
-          //   },
-          // ),
-          CustomTextField(
-            hintText: 'İsim',
-            controller: widget.nameController,
-            prefixIcon: const Icon(Icons.person),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Lütfen bir isim girin';
-              }
-              return null;
-            },
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-          ),
-          const SizedBox(height: 16),
-          // TextFormField(
-          //   controller: widget.emailController,
-          //   decoration: const InputDecoration(
-          //     labelText: 'E-posta',
-          //     prefixIcon: Icon(Icons.email),
-          //   ),
-          //   validator: (value) {
-          //     if (value == null || value.isEmpty) {
-          //       return 'Lütfen bir e-posta girin';
-          //     }
-          //     if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-          //       return 'Geçerli bir e-posta adresi girin';
-          //     }
-          //     return null;
-          //   },
-          // ),
-          CustomTextField(
-            hintText: 'E-posta',
-            controller: widget.emailController,
-            prefixIcon: const Icon(Icons.email),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Lütfen bir e-posta girin';
-              }
-              if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-                return 'Geçerli bir e-posta adresi girin';
-              }
-              return null;
-            },
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-          ),
-          const SizedBox(height: 16),
-          // TextFormField(
-          //   controller: widget.phoneController,
-          //   decoration: const InputDecoration(
-          //     labelText: 'Telefon',
-          //     prefixIcon: Icon(Icons.phone),
-          //   ),
-          //   validator: (value) {
-          //     if (value == null || value.isEmpty) {
-          //       return 'Lütfen bir telefon numarası girin';
-          //     }
-          //     if (!RegExp(r'^\+?[\d\s-]{10,}$').hasMatch(value)) {
-          //       return 'Geçerli bir telefon numarası girin';
-          //     }
-          //     return null;
-          //   },
-          // ),
-          CustomTextField(
-            hintText: 'Telefon',
-            controller: widget.phoneController,
-            keyboardType: TextInputType.phone,
-            prefixIcon: const Icon(Icons.phone),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Lütfen bir telefon numarası girin';
-              }
-              if (!RegExp(r'^\+?[\d\s-]{10,}$').hasMatch(value)) {
-                return 'Geçerli bir telefon numarası girin';
-              }
-              return null;
-            },
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // TextFormField(
+            //   controller: widget.nameController,
+            //   decoration: const InputDecoration(
+            //     labelText: 'İsim',
+            //     prefixIcon: Icon(Icons.person),
+            //   ),
+            //   validator: (value) {
+            //     if (value == null || value.isEmpty) {
+            //       return 'Lütfen bir isim girin';
+            //     }
+            //     return null;
+            //   },
+            // ),
+            CustomTextField(
+              hintText: 'İsim',
+              controller: widget.nameController,
+              prefixIcon: const Icon(Icons.person),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Lütfen bir isim girin';
+                }
+                return null;
+              },
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            ),
+            const SizedBox(height: 16),
+            // TextFormField(
+            //   controller: widget.emailController,
+            //   decoration: const InputDecoration(
+            //     labelText: 'E-posta',
+            //     prefixIcon: Icon(Icons.email),
+            //   ),
+            //   validator: (value) {
+            //     if (value == null || value.isEmpty) {
+            //       return 'Lütfen bir e-posta girin';
+            //     }
+            //     if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+            //       return 'Geçerli bir e-posta adresi girin';
+            //     }
+            //     return null;
+            //   },
+            // ),
+            CustomTextField(
+              hintText: 'E-posta',
+              controller: widget.emailController,
+              prefixIcon: const Icon(Icons.email),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Lütfen bir e-posta girin';
+                }
+                if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                    .hasMatch(value)) {
+                  return 'Geçerli bir e-posta adresi girin';
+                }
+                return null;
+              },
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            ),
+            const SizedBox(height: 16),
+            // TextFormField(
+            //   controller: widget.phoneController,
+            //   decoration: const InputDecoration(
+            //     labelText: 'Telefon',
+            //     prefixIcon: Icon(Icons.phone),
+            //   ),
+            //   validator: (value) {
+            //     if (value == null || value.isEmpty) {
+            //       return 'Lütfen bir telefon numarası girin';
+            //     }
+            //     if (!RegExp(r'^\+?[\d\s-]{10,}$').hasMatch(value)) {
+            //       return 'Geçerli bir telefon numarası girin';
+            //     }
+            //     return null;
+            //   },
+            // ),
+            CustomTextField(
+              hintText: 'Telefon',
+              controller: widget.phoneController,
+              keyboardType: TextInputType.phone,
+              prefixIcon: const Icon(Icons.phone),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Lütfen bir telefon numarası girin';
+                }
+                if (!RegExp(r'^\+?[\d\s-]{10,}$').hasMatch(value)) {
+                  return 'Geçerli bir telefon numarası girin';
+                }
+                return null;
+              },
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+            ),
+          ],
+        ),
       ),
     );
   }
